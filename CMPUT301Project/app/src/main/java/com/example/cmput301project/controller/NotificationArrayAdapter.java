df56d1c71e43e913a8dc6a8f2f35bc855077ce48
Xinjia483 <xjfan2020@outlook.com>
2024-11-14 09:15:12 -0700

    set up notification

--- CMPUT301Project/app/src/main/java/com/example/cmput301project/controller/NotificationArrayAdapter.java
+++ CMPUT301Project/app/src/main/java/com/example/cmput301project/controller/NotificationArrayAdapter.java
@@ -1,10 +1,20 @@
 package com.example.cmput301project.controller;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
+import com.example.cmput301project.R;
 import com.example.cmput301project.model.Entrant;
 import com.example.cmput301project.model.Notification;
 
@@ -14,5 +24,24 @@ import java.util.List;
 public class NotificationArrayAdapter extends ArrayAdapter<Notification> {
     public NotificationArrayAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Notification> objects) {
         super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.notification_list_item,
+                    parent, false);
+        } else {
+            view = convertView;
+        }
+        Notification n = getItem(position);
+        TextView message = view.findViewById(R.id.notification_message);
+        if (n != null) {
+            message.setText(n.getMessage());
+        }
+
+        return view;
     }
 }
