32db32ff36760e3654397d9228437cc12c00d30d
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2024-11-22 00:55:25 -0700

    admin changes

--- CMPUT301Project/app/src/main/java/com/example/cmput301project/view/AdminDashboardActivity.java
+++ CMPUT301Project/app/src/main/java/com/example/cmput301project/view/AdminDashboardActivity.java
@@ -39,8 +39,10 @@ public class AdminDashboardActivity extends AppCompatActivity {
             Log.d("AdminDashboardActivity", "Intent to AdminAllEventsActivity started");
         });
 
-        manageImagesButton.setOnClickListener(v -> {
-            // TODO: Navigate to images management
+        manageImagesButton.setOnClickListener(view -> {
+            Log.d("AdminDashboardActivity", "Manage Images button clicked");
+            Intent intent = new Intent(AdminDashboardActivity.this, AdminManageImagesActivity.class);
+            startActivity(intent);
         });
 
         manageQrButton.setOnClickListener(v -> {
