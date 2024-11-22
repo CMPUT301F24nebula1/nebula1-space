a68f8a9cc81f696f0659f200d9620f1bd344bfa7
Xinjia483 <xjfan2020@outlook.com>
2024-11-22 13:34:37 -0700

    US 01.04.03 opt out of receiving notifications

--- CMPUT301Project/app/src/main/java/com/example/cmput301project/model/Entrant.java
+++ CMPUT301Project/app/src/main/java/com/example/cmput301project/model/Entrant.java
@@ -18,7 +18,7 @@ public class Entrant extends User {
     private String profilePictureUrl;
     private String status;
     private String initials;
-    private Boolean receiveNotification;
+    private Boolean receiveNotification = true;
 
     /**
      * Default constructor initializing an empty waitlist.
@@ -169,6 +169,10 @@ public class Entrant extends User {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public  boolean getReceiveNotification() { return receiveNotification; }
+
+    public  void setReceiveNotification(Boolean receiveNotification) {this.receiveNotification = receiveNotification; }
 
     public ArrayList<Notification> getNotifications() {
         return notifications;
